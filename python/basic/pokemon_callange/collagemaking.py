b5ce8f2e37757336dba99997dc963b4322aab99c
ShubhamPal19 <shubhampal2309@gmail.com>
2021-08-25 11:28:28 +0530

    pokemone collage making challenge

---
+++ python/basic/pokemon_callange/collagemaking.py
@@ -0,0 +1,76 @@
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+print(34234235235)
+c=cv2.imread("center.jpeg")
+# tl=cv2.imread("top_left.jpg")
+# tr=cv2.imread("top_right.jpg")
+# br=cv2.imread("bottom_right.jpg")
+# bl=cv2.imread("bottom_left.jpg")
+cv2.imshow("win",c)
+print(34234235235)
+
+cv2.waitKey(100)
+
+cv2.resize()
+
+
+
+# rgb_c  = cv2.cvtColor(c,cv2.COLOR_BGR2RGB)
+# rgb_tl  = cv2.cvtColor(tl,cv2.COLOR_BGR2RGB)
+# rgb_tr  = cv2.cvtColor(tr,cv2.COLOR_BGR2RGB)
+# rgb_br  = cv2.cvtColor(br,cv2.COLOR_BGR2RGB)
+# rgb_bl  = cv2.cvtColor(bl,cv2.COLOR_BGR2RGB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# c= cv2.cvtColor(cv2.imread("center.jpeg"),cv2.COLOR_BGR2RGB)
+# print("helo")
+# tl=cv2.cvtColor(cv2.imread("top_left.jpg"),cv2.COLOR_BGR2RGB)
+# tr=cv2.cvtColor(cv2.imread("top_right.jpg"),cv2.COLOR_BGR2RGB)
+# br= cv2.cvtColor(cv2.imread("bottom_right.jpg"),cv2.COLOR_BGR2RGB)
+# bl= cv2.cvtColor(cv2.imread("bottom_left.jpg"),cv2.COLOR_BGR2RGB)
+
+
+# tl.resize((400,400))
+# tr.resize((400,400))
+# bl.resize((400,400))
+# br.resize((400,400))
+
+
+
+
+# top=cv2.hconcat([tl,tr])
+# bottom=cv2.hconcat([bl,br])
+
+# backgnd=cv2.vconcat([top,bottom])
+
+
+
+# cv2.imshow("win",backgnd)
+
+
+
+
